d1dfffa749f60ffb49f5f0a601f1b15be841569d
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-06-01 11:08:22 +0530

    deleted previous files

--- Corejava/src/com/javaoops/abstraction/Services.java
+++
@@ -1,9 +0,0 @@
-package com.javaoops.abstraction;
-
-public interface Services {
-	int sumsurvice(int a,int b);
-	int subService(int a,int b);
-	int multService(int a,int b);
-	int divService(int a,int b);
-	int modulous(int a,int b);
-}
