87b55c32b0d9ba4162957481e8cad016f76269e3
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-06-01 13:16:29 +0530

    upperlowercase methods

---
+++ String_handling/upperLowercase.java
@@ -0,0 +1,18 @@
+package String_handling;
+
+public class upperLowercase 
+{
+    public static void main(String[] args) 
+	{
+		String str1="Kanhaiya";
+		String str2="ManiSH";
+		String y=str1.toUpperCase();
+		String z=str2.toLowerCase();
+		System.out.println("Printing upper case letters");
+		System.out.println(y);
+		System.out.println("Printing lower case letters");
+		System.out.println(z);
+		
+	}
+    
+}
