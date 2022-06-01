63ea31ab9b3223924200a8c13703b4a3327a017a
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-06-01 13:09:44 +0530

    substring method

---
+++ String_handling/subString.java
@@ -0,0 +1,16 @@
+package String_handling;
+
+public class subString 
+{
+    public static void main(String[] args) 
+	{
+		String str1="International";
+		int z=str1.length();
+		String x=str1.substring(5);
+		String y=str1.substring(5,11);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+    
+}
