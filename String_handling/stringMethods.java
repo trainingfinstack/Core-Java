321f1460e1ae8b030cbb1cec29579f7ae4222618
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-06-01 12:29:27 +0530

    .length method of string

---
+++ String_handling/stringMethods.java
@@ -0,0 +1,13 @@
+package String_handling;
+
+public class stringMethods 
+{
+    public static void main(String[] args)
+    {
+        String str1="Welcome to the digital world";
+        int x=str1.length();
+        System.out.println(x);
+        
+    }
+    
+}
