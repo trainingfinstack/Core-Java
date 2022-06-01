709e1b574191be4620f71efb924b0f51a1c41788
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-06-01 14:26:49 +0530

    string values comparison

---
+++ String_handling/stringEquals.java
@@ -0,0 +1,18 @@
+package String_handling;
+
+public class stringEquals
+{
+    public static void main(String[] args) 
+	{
+		String str1="Kanhaiyakumar991";
+		String str2="Kanhaiyakumar991";
+		String str3=new String("cetpa");
+		String str4=new String("cetpa");
+		boolean st=str1==str2;
+		boolean st1=str3==str4;
+		System.out.println(st);
+		System.out.println(st1);
+		
+		
+	}
+}
