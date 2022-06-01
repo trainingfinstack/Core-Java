911378916a756221b610fac5f6041812d6de3845
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-06-01 14:17:00 +0530

    show month added

---
+++ String_handling/showMonth.java
@@ -0,0 +1,13 @@
+package String_handling;
+
+public class showMonth 
+{
+    public static void main(String[] args) 
+    {
+        String dob= "17/12/1997";
+        String [] ar=dob.split("/");
+        System.out.println(ar[1]);
+        
+    }
+    
+}
