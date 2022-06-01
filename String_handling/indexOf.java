09180f606118b5a72264d4736587cfa456562870
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-06-01 13:02:33 +0530

    indexOf method

---
+++ String_handling/indexOf.java
@@ -0,0 +1,15 @@
+package String_handling;
+
+public enum indexOf 
+{
+    public static void main(String[] args) 
+	{
+		String str1="Kanhaiya";
+		int ch=str1.indexOf('h');
+		int bh=str1.lastIndexOf('a');
+		System.out.println(ch);
+		System.out.println(bh);
+		
+	}
+    
+}
