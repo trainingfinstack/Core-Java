fc402429bacbf3e44465830dd76f921a2eab64d7
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-06-01 11:11:52 +0530

    programme to print default value

---
+++ defaultValue.java
@@ -0,0 +1,16 @@
+
+public class defaultValue
+{
+	public static void main(String[] args) 
+	{
+		int [] arr= {10,20,30,40,50};
+		String [] str=new String [6];
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+			System.out.println(str[i]);
+		}
+
+	}
+
+}
