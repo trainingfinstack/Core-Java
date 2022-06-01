14b887e8d9bc30ac86d0b189bb5657e25c678e29
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-06-01 13:11:55 +0530

    substring methods

--- String_handling/subString.java
+++ String_handling/subString.java
@@ -7,6 +7,7 @@ public class subString
 		String str1="International";
 		int z=str1.length();
 		String x=str1.substring(5);
+        //this will print the index of 5 to 10
 		String y=str1.substring(5,11);
 		System.out.println(x);
 		System.out.println(y);
