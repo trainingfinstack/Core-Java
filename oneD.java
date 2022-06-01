68e6c92e72fd416008956659fd9d11ec24d20a9e
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-06-01 11:14:00 +0530

    programmes added

---
+++ oneD.java
@@ -0,0 +1,16 @@
+
+public class oneD 
+{
+	public static void main(String[] args) 
+	{
+		int a[];
+		int a1[]= {1,2,3,4,5};
+		int []a2,b; //here both a2 & b are int type of array
+		int a3[],b1; //here a3 is an array but b is int type of variables
+		int [] arr=new int[5];
+		
+		
+
+	}
+
+}
---
+++ rectangle.java
@@ -0,0 +1,15 @@
+
+public class rectangle 
+{
+	private double length;
+	 
+	public double getlength()
+	{
+		return length;
+	}
+	public void setlength(double l)
+	{
+		length=l;
+	}
+
+}
