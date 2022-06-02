bd1e1d7bc82e515b3877544606ba2441059fabef
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-06-02 11:48:37 +0530

    using lambda expression

--- lambdaExpression/appMain.java
+++ lambdaExpression/appMain.java
@@ -10,30 +10,36 @@ public class appMain
     //  lambdaimplements i=new lambdaimplements();
     //  i.sayHello();
     //or
-        myinterface i=new myinterface()
-         {
+    //     myinterface i=new myinterface()
+    //      {
 
-            @Override
-            public void sayHello() 
-            {
-               System.out.println("This is the first anonymous class without implementing the interface");
+    //         @Override
+    //         public void sayHello() 
+    //         {
+    //            System.out.println("This is the first anonymous class without implementing the interface");
                 
-            }
+    //         }
             
-        };
-        i.sayHello();
-        myinterface i1=new myinterface()
-        {
+    //     };
+    //     i.sayHello();
+    //     myinterface i1=new myinterface()
+    //     {
 
-           @Override
-           public void sayHello() 
-           {
-              System.out.println("This is the secnd anonymous class without implementing the interface");
+    //        @Override
+    //        public void sayHello() 
+    //        {
+    //           System.out.println("This is the secnd anonymous class without implementing the interface");
                
-           }
+    //        }
            
-       };
-       i1.sayHello();
+    //    };
+    //    i1.sayHello();
+
+    //using lambda function
+    myinterface i= ()-> System.out.println("This is first using lambda Expression");
+    myinterface i1= ()-> System.out.println("This is second using lambda Expression");
+    i.sayHello();
+    i1.sayHello();
     }
     
 }
