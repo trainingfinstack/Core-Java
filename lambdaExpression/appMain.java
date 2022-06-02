a1c23aec8c6a314e375ce2adc3e03a92d099f00d
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-06-02 11:43:11 +0530

    second commit creating anonymous class without using any other class

--- lambdaExpression/appMain.java
+++ lambdaExpression/appMain.java
@@ -7,10 +7,33 @@ public class appMain
      System.out.println("This is print from main method");  
        
      //using anonymous class implements interface
-     lambdaimplements i=new lambdaimplements();
-     i.sayHello();
-  
-        
+    //  lambdaimplements i=new lambdaimplements();
+    //  i.sayHello();
+    //or
+        myinterface i=new myinterface()
+         {
+
+            @Override
+            public void sayHello() 
+            {
+               System.out.println("This is the first anonymous class without implementing the interface");
+                
+            }
+            
+        };
+        i.sayHello();
+        myinterface i1=new myinterface()
+        {
+
+           @Override
+           public void sayHello() 
+           {
+              System.out.println("This is the secnd anonymous class without implementing the interface");
+               
+           }
+           
+       };
+       i1.sayHello();
     }
     
 }
