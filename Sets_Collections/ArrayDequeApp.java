b92adf14a773d83e150523ea017de47e8f894a0b
Kanhaiya Kumar <5kanhaiyakumar@gmail.com>
2022-06-07 17:24:38 +0530

    ArrayDeque

---
+++ Sets_Collections/ArrayDequeApp.java
@@ -0,0 +1,21 @@
+package Sets_Collections;
+import java.util.ArrayDeque;
+import java.util.*;
+public class ArrayDequeApp 
+{
+    public static void main(String[] args) 
+    {
+        ArrayDeque<Integer> ad1=new ArrayDeque<Integer>();
+		ad1.add(23);
+		ad1.add(24);
+		ad1.offerLast(34);
+		ad1.offerLast(56);
+		ad1.offerLast(45);
+		ad1.offerLast(30);  //this method is available in this ArrayDeque interface only
+		ad1.offerFirst(22);
+		ad1.offerFirst(45); //this method is available in this ArrayDeque interface only
+		ad1.offerFirst(56);
+		System.out.println(ad1);
+    }
+    
+}
