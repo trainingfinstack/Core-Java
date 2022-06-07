cef07b13ea45a00dd0bb7500ad1f2d8443a847ec
Kanhaiya Kumar <5kanhaiyakumar@gmail.com>
2022-06-07 12:18:54 +0530

    hashset practise

---
+++ Sets_Collections/HashsetPractise1.java
@@ -0,0 +1,25 @@
+package Sets_Collections;
+import java.util.HashSet;
+import java.util.*;
+
+public class HashsetPractise1 
+{
+    public static void main(String[] args) 
+    {
+        HashSet<Integer> evenNumber=new HashSet<Integer>();//by defualt capacity will be 16 and load factor will be 0.75 i.e 75%
+		// Using add() method
+        evenNumber.add(2);
+        evenNumber.add(4);
+        evenNumber.add(6);
+        System.out.println("HashSet: " + evenNumber);
+
+        HashSet<Integer> numbers = new HashSet<>();
+        
+        // Using addAll() method
+        numbers.addAll(evenNumber);
+        numbers.add(5);
+        System.out.println("New HashSet: " + numbers);
+        
+    }
+    
+}
