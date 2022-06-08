492ef19bde080867750c8876fdd1c388a66940c4
Kanhaiya Kumar <5kanhaiyakumar@gmail.com>
2022-06-08 16:00:45 +0530

    iterable methods added

---
+++ Sets_Collections/iteratorMethods.java
@@ -0,0 +1,41 @@
+package Sets_Collections;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.*;
+public class iteratorMethods 
+{
+    public static void main(String[] args) 
+    {
+        ArrayList<Integer> numbers=new ArrayList<Integer>();
+		numbers.add(1);
+		numbers.add(3);
+		numbers.add(2);
+//		System.out.println("ArrayList: "+numbers);
+//		
+//		//creating an instance of iterator
+		Iterator<Integer> iterate=numbers.iterator();
+//		
+		//using the next() method
+//		int number=iterate.next();
+//		System.out.println("Accessed Element: "+number);
+//		
+		//using the remove() method
+//		iterate.remove();
+//		System.out.println("Removed Element:" +number);
+//		
+//		System.out.println("Updated ArrayList: ");
+//		
+		//Using the hasNext() method
+		while(iterate.hasNext())
+		{
+			//using the next forEachRemaning Method
+			iterate.forEachRemaining((value) -> System.out.print(value + ","));
+		}
+		
+
+	}
+
+        
+    }
+    
+}
